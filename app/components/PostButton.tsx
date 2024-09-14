e59733bf332409f07b08a4a8ad839ddd52557385
yuta925 <yutarou041@gmail.com>
2024-09-14 17:45:01 +0900

    投稿画面へ遷移するボタンの作成

--- app/components/PostButton.tsx
+++ app/components/PostButton.tsx
@@ -1,5 +1,17 @@
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import { Button } from "@mui/material";
+import Link from "next/link";
+
 const PostButton = () => {
-  return <div>PostButton</div>;
+  return (
+    <div>
+      <Link href="/post">
+        <Button>
+          <AddCircleOutlineIcon className="size-12" />
+        </Button>
+      </Link>
+    </div>
+  );
 };
 
 export default PostButton;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 import Comments from "./components/Comments";
+import PostButton from "./components/PostButton";
 
 export default function Home() {
   return (
     <div className="">
       <Comments />
+      {/* PostButton を画面の右下に固定 */}
+      <div className="fixed bottom-4 right-4 z-10">
+        <PostButton />
+      </div>
     </div>
   );
 }
