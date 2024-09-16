f8e0a3fae138b34402e3bdc47fe0280ac83a3104
ojiro-doi <sakura.0046@icloud.com>
2024-09-16 11:46:58 +0900

    細かなUI改善

--- app/components/HeaderLogo.tsx
+++ app/components/HeaderLogo.tsx
@@ -3,17 +3,17 @@ import React from "react";
 const HeaderLogo = () => {
   const iconSrc =
     "https://media.connpass.com/thumbs/65/ab/65ab2b7f912660bbc6f1af755788c43c.png";
-  const name = "ロゴ";
+  const name = "Tech.SummerCamp";
   return (
     <a href="/home" className="flex items-center space-x-2 ml-4">
       <img
         src={iconSrc}
-        alt="ロゴ"
+        alt="logo"
         width={32}
         height={32}
         className="rounded-lg"
       />
-      <span className="text-xl font-bold">{name}</span>
+      <span className="text-base font-bold">{name}</span>
     </a>
   );
 };
--- app/post/page.tsx
+++ app/post/page.tsx
@@ -12,125 +12,123 @@ import HashTagUI from "../components/AddHash";
 import { RequestBodyToPostType } from "../types/postEvent";
 
 const Post = () => {
-	const [formData, setFormData] = useState<RequestBodyToPostType>({
-		description: "",
-		event_date: "",
-		location: "",
-		max_participants: 0,
-		min_participants: 0,
-		name: "",
-		tags: [],
-	});
+  const [formData, setFormData] = useState<RequestBodyToPostType>({
+    description: "",
+    event_date: "",
+    location: "",
+    max_participants: 0,
+    min_participants: 0,
+    name: "",
+    tags: [],
+  });
 
-	const handleChange = (
-		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-	) => {
-		const { name, value } = e.target;
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
 
-		// 数値フィールドの処理
-		const numericFields = ["max_participants", "min_participants"];
-		const parsedValue = numericFields.includes(name)
-			? Number(value)
-			: value;
+    // 数値フィールドの処理
+    const numericFields = ["max_participants", "min_participants"];
+    const parsedValue = numericFields.includes(name) ? Number(value) : value;
 
-		setFormData((prevData) => ({
-			...prevData,
-			[name]: parsedValue,
-		}));
-	};
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: parsedValue,
+    }));
+  };
 
-	// タグが更新されたときの処理
-	const handleAddTag = (tags: string[]) => {
-		setFormData((prevData) => ({
-			...prevData,
-			tags: tags,
-		}));
-	};
+  // タグが更新されたときの処理
+  const handleAddTag = (tags: string[]) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      tags: tags,
+    }));
+  };
 
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		console.log("Submitted Data:", formData);
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Submitted Data:", formData);
 
-		try {
-			const response = await axios.post(
-				"https://mock.apidog.com/m1/666106-637317-default/groups/3307bda6-61f6-480f-af5d-eec2f15a1897/events",
-				formData
-			);
-			console.log("サーバーからのレスポンス:", response.data);
-		} catch (error) {
-			console.error("エラーが発生しました:", error);
-		}
-	};
+    try {
+      const response = await axios.post(
+        "https://mock.apidog.com/m1/666106-637317-default/groups/3307bda6-61f6-480f-af5d-eec2f15a1897/events",
+        formData
+      );
+      console.log("サーバーからのレスポンス:", response.data);
+    } catch (error) {
+      console.error("エラーが発生しました:", error);
+    }
+  };
 
-	return (
-		<>
-			<RouteHeader title={"投稿"} prePath={"/home"} />
-			<div className="flex items-center justify-center w-full">
-				<form onSubmit={handleSubmit}>
-					<div className="mb-12 w-96">
-						<div className="my-2 w-full">
-							<p className="font-bold text-lg p-1">タグ追加</p>
-							<HashTagUI onAddTag={handleAddTag} />
-						</div>
-						<div className="my-2 w-full">
-							<BaseInput
-								title="タイトル"
-								name="name"
-								value={formData.name}
-								onChange={handleChange}
-							/>
-						</div>
-						<div className="my-2 w-full">
-							<BaseInput
-								title="内容"
-								name="description"
-								value={formData.description}
-								onChange={handleChange}
-							/>
-						</div>
-					</div>
-					<div className="mb-24">
-						<p className="font-bold text-2xl">追加情報を入力</p>
-						<div className="flex items-center justify-between my-8 pr-20">
-							<CalendarTitle />
-							<input
-								type="date"
-								name="event_date"
-								className="border w-36"
-								value={formData.event_date}
-								onChange={handleChange}
-							/>
-						</div>
-						<div className="flex items-center justify-between my-8 pr-20">
-							<PlaceTitle />
-							<input
-								type="text"
-								name="location"
-								className="border w-36"
-								value={formData.location}
-								onChange={handleChange}
-							/>
-						</div>
-						<div className="flex items-center justify-between my-8 pr-20">
-							<PeopleTitle />
-							<input
-								type="range"
-								name="max_participants"
-								min={0}
-								max="100"
-								className="range w-36"
-								value={formData.max_participants}
-								onChange={handleChange}
-							/>
-						</div>
-					</div>
-					<div className="flex justify-center">
-						<CreateButton name={"作成する"} />
-					</div>
-				</form>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <RouteHeader title={"投稿"} prePath={"/home"} />
+      <div className="flex items-center justify-center w-full">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-12 w-96">
+            <div className="my-2 w-full">
+              <p className="font-bold text-lg p-1">タグ追加</p>
+              <HashTagUI onAddTag={handleAddTag} />
+            </div>
+            <div className="my-2 w-full">
+              <BaseInput
+                title="タイトル"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="my-2 w-full">
+              <BaseInput
+                title="内容"
+                name="description"
+                value={formData.description}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="mb-24">
+            <p className="font-bold text-2xl">追加情報を入力</p>
+            <div className="flex items-center justify-between my-8 pr-20">
+              <CalendarTitle />
+              <input
+                type="date"
+                name="event_date"
+                className="border w-36"
+                value={formData.event_date}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex items-center justify-between my-8 pr-20">
+              <PlaceTitle />
+              <input
+                type="text"
+                name="location"
+                className="border w-36"
+                value={formData.location}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex items-center justify-between my-8 pr-20">
+              <PeopleTitle />
+              <input
+                type="range"
+                name="max_participants"
+                min={0}
+                max="100"
+                className="range w-36"
+                value={formData.max_participants}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="flex justify-center mb-8">
+            <CreateButton name={"作成する"} />
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Post;
